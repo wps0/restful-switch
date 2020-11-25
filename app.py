724d4c8ae59a22fb7df0bfc3c2104545a88d5b3e
PW <53907986+wps0@users.noreply.github.com>
2020-11-25 16:48:09 +0100

    hello world

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app, "/v1")
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Homepage(Resource):
+    def get(self):
+        return 'Hello World!'
+
+
+api.add_resource(Homepage, "/")
 
 
 if __name__ == '__main__':
